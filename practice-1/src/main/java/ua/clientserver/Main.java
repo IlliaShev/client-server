7bab7ef53f833e6a6ba9c970cb4304462cef974b
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2022-06-07 21:49:43 +0200

    practice-1 skeleton

---
+++ practice-1/src/main/java/ua/clientserver/Main.java
@@ -0,0 +1,9 @@
+package ua.clientserver;
+
+public class Main {
+    public static void main(String[] args) {
+        Message message = new Message();
+        message.setMessage("Hello world!");
+        System.out.println(message);
+    }
+}
---
+++ practice-1/src/main/java/ua/clientserver/Message.java
@@ -0,0 +1,12 @@
+package ua.clientserver;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Message {
+
+    private String message;
+
+}
